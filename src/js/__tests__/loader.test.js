408d8cffa72cbc28323cf299d01749294ddaa5a7
Dolinin2021 <programmistdolinin@mail.ru>
2023-03-14 16:13:28 +0600

    edit file loader.test.js

--- src/js/__tests__/loader.test.js
+++ src/js/__tests__/loader.test.js
@@ -1,7 +1,8 @@
 import GameSavingLoader from '../loader';
 
-test('works with promises', () => {
+test('works with promises', async () => {
   expect.assertions(1);
+  const data = await GameSavingLoader.load();
   const expected = '{"id":9,"created":1546300800,"userInfo":{"id":1,"name":"Hitman","level":10,"points":2000}}';
-  return GameSavingLoader.load().then((saving) => expect(saving).toEqual(expected));
+  expect(data).toEqual(expected);
 });
