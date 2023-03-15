7746679648fc51158e4505001414ebc0436bda2a
Dolinin2021 <programmistdolinin@mail.ru>
2023-03-15 21:07:23 +0600

    fix

--- src/js/__tests__/loader.test.js
+++ src/js/__tests__/loader.test.js
@@ -3,6 +3,12 @@ import GameSavingLoader from '../loader';
 test('works with promises', async () => {
   expect.assertions(1);
   const data = await GameSavingLoader.load();
-  const expected = '{"id":9,"created":1546300800,"userInfo":{"id":1,"name":"Hitman","level":10,"points":2000}}';
+  const expected = {
+    id: 9,
+    created: 1546300800,
+    userInfo: {
+      id: 1, name: 'Hitman', level: 10, points: 2000,
+    },
+  };
   expect(data).toEqual(expected);
 });
--- src/js/loader.js
+++ src/js/loader.js
@@ -1,9 +1,15 @@
 import read from './reader';
 import json from './parser';
+import GameSaving from './saving';
 
 export default class GameSavingLoader {
   static load() {
     const data = read();
-    return data.then((data) => json(data)).then((value) => value);
+    return data.then((data) => json(data))
+      .then((value) => {
+        const res = JSON.parse(value);
+        const instance = new GameSaving(res.id, res.created, res.userInfo);
+        return instance;
+      });
   }
 }
